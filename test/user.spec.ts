9285d0d71504974f4b96dbf1b5c57462aabfe8a7
Rizky Kurniawan <arkisme9997@gmail.com>
2024-04-25 16:29:03 +0700

    add test user module & service

--- test/user.spec.ts
+++ test/user.spec.ts
@@ -4,22 +4,30 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { TestService } from './test.service';
+import { TestModule } from './test.module';
 
 describe('UserController', () => {
   let app: INestApplication;
   let logger: Logger;
+  let testService: TestService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, TestModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
     logger = app.get(WINSTON_MODULE_PROVIDER);
+    testService = app.get(TestService);
   });
 
   describe('POST /api/users', () => {
+    beforeEach(async () => {
+      await testService.deleteUser();
+    });
+
     it('should be rejected if request is invalid', async () => {
       const response = await request(app.getHttpServer())
         .post('/api/users')
@@ -39,16 +47,32 @@ describe('UserController', () => {
       const response = await request(app.getHttpServer())
         .post('/api/users')
         .send({
-          username: 'testAplikasi2',
+          username: 'testAplikasi',
           password: '12345678',
           password_confirmation: '12345678',
-          name: 'Test Aplikasi 2',
+          name: 'Test Aplikasi',
         });
 
       logger.info(response.body);
       expect(response.status).toBe(201);
-      expect(response.body.data.username).toBe('testAplikasi2');
-      expect(response.body.data.name).toBe('Test Aplikasi 2');
+      expect(response.body.data.username).toBe('testAplikasi');
+      expect(response.body.data.name).toBe('Test Aplikasi');
+    });
+
+    it('should be able to register', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: 'testAplikasi',
+          password: '12345678',
+          password_confirmation: '12345678',
+          name: 'Test Aplikasi',
+        });
+
+      logger.info(response.body);
+      expect(response.status).toBe(201);
+      expect(response.body.data.username).toBe('testAplikasi');
+      expect(response.body.data.name).toBe('Test Aplikasi');
     });
   });
 });
